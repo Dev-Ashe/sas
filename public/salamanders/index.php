f96cb034e879544a3e9088ad7040f765c34ce9b7
Devin Ashe <devashe98@aol.com>
2023-03-09 19:55:06 -0500

    "change-1"

--- public/salamanders/index.php
+++ public/salamanders/index.php
@@ -1,20 +1,27 @@
 <!-- require initialize.php -->
+<?php require_once('../../private/initialize.php'); ?>
+
+<?php include(SHARED_PATH . '/salamander-header.php'); 
+
+$pageTitle = 'Salamanders';
+
+$salamanders = [
+  ['id' => '1', 'salamanderName' => "Red-Legged Salamander"],
+  ['id' => '2', 'salamanderName' => "Pigeon Mountain Salamander"],
+  ['id' => '3', 'salamanderName' => "ZigZag Salamander"],
+  ['id' => '4', 'salamanderName' => "Slimy Salamander"]
+];
 
 
-<!-- 
-  Write a salamanders array with the following
+?>
+
+
+  <!-- Write a salamanders array with the following
 id=1, salamanderName = Red-Legged Salamander
 id=2, salamanderName = Pigeon Mountain Salamander
 id=3', salamanderName = ZigZag Salamander
-id=4,  salamanderName= Slimy Salamander 
--->
+id=4,  salamanderName= Slimy Salamander  -->
 
-
-
-<!-- Add the pageTitle for salamanders
-Include a shared path to the salamander header -->
-
-?>
 
 <h1>Salamanders</h1>
 
@@ -28,16 +35,19 @@ Include a shared path to the salamander header -->
     <th>&nbsp;</th>
     <th>&nbsp;</th>
   </tr>
+        <!-- Use url_for with show.php AND h(u) with the salamander['id'] -->
 
       <?php foreach($salamanders as $salamander) { ?>
         <tr>
-          <!-- <td>Display the salamander id</td> -->
-    	    <!-- <td>Display the salamander name</td> -->
-          <!-- Use url_for with show.php AND h(u) with the salamander['id'] -->
+          <td><?= h($salamander['id']); ?></td>
+          <td><?= h($salamander['salamanderName']); ?></td>
+          <td><a href="<?= urlFor('salamanders/show.php?id=' . h(u($salamander['id']))); ?>">View</a></td>
           <td><a href="#">Edit</a></td>
           <td><a href="#">Delete</a></td>
-    	  </tr>
+        </tr>
       <?php } ?>
+        
+     ?>
   	</table>
 
-<!-- <?php add the shared path to the salamander footer ?> -->
+<?php include(SHARED_PATH . '/salamander-footer.php'); ?>
